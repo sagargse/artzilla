503f9b34dafc053f369870cdef92c993cbb0486c
sagargse <sagarraj20008@gmail.com>
2022-04-08 12:03:04 +0530

    fix: change css and add some cards

--- js files/components.js
+++ js files/components.js
@@ -4,3 +4,10 @@ const closeBtn = document.querySelector("#close-btn");
 closeBtn.addEventListener("click",()=>{
     alertClose.style.display = "none";
 })
+
+const card = document.getElementById("card-dismiss");
+const dismissButton = document.querySelector(".card-dismiss-icon");
+
+dismissButton.addEventListener("click", () => {
+  card.style.display = "none";
+});
